827413d9caa6c24a3a00bffefafe602660498bc0
Mohd-Sami819 <mohdshami8199@gmail.com>
2025-10-24 00:40:40 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,95 +1,20 @@
-import { readFile, writeFile } from 'fs/promises';
-import path from 'path';
-import crypto from 'crypto';
 import express from 'express'
- 
+import { shortenedRoutes } from './routes/shortener.routes.js'; 
+
 const app = express();
 
 
 const PORT = process.env.PORT || 3000;
-const DATA_FILE = path.join("data", 'links.json')
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 
-
-const loadLinks = async () => {
-    try {
-        const data = await readFile(DATA_FILE, 'utf-8');
-        return JSON.parse(data);
-    } catch (error) {
-        if (error.code === "ENOENT") {
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {}
-        }
-        throw error;
-    }
-};
-
-const saveLinks = async (links) => {
-    await writeFile(DATA_FILE, JSON.stringify(links));
-}
+app.set("view engine","ejs");
+app.use(shortenedRoutes)
 
 
-app.get("/", async (req, res) => {
-    try {
-        const file = await readFile(path.join("views", "index.html"));
-        const links = await loadLinks();
-
-        const content = file.toString().replaceAll(
-            "{{shortened_urls}}",
-             Object.entries(links)
-        .map(
-            ([shortCode,url]) => 
-                `<li><a href="/${shortCode}" target="_blank">${req.headers.host}/${shortCode}</a> → ${url}</li>`
-        ).join("")
-    );
-    return res.send(content)
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send("Internal server error");
-    }
-});
-
-app.post("/", async (req, res) => {
-    try {
-        const { url, shortCode } = req.body;
-        const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-
-        const links = await loadLinks();
-
-        if (links[finalShortCode]) {
-            return res.status(400).send("Short code already exists. Please choose another");
-        }
-
-        links[finalShortCode] = url;
-
-        await saveLinks(links);
-        return res.redirect("/");
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error")
-    }
-})
-
-app.get("/:shortCode",async(req,res)=>{
-    try {
-        const {shortCode} = req.params;
-        const links = await loadLinks();
-
-        if (!links[shortCode]) {
-            return res.status(404).send("404 error occurred");
-        }
-        return res.redirect(links[shortCode]);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send("Internal server error")
-    }
-})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
-
-})  
+})
